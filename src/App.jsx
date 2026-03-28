9c83b2c0a513b577a4d2bf7451e118b57fed49d3
Tania Herasymenko <tanyagera2003@gmail.com>
2026-03-28 15:55:21 +0200

    Add: code compliler

--- src/App.jsx
+++ src/App.jsx
@@ -14,47 +14,56 @@ function App() {
   }
 
   return (
-    <div className={`app${current ? " app--started" : ""}`}>
-      <div className="header">
-        <h1>Algorithm Problem Randomizer</h1>
-        <button className="roll-btn" onClick={getRandomProblem}>
-          {current ? "Next Problem" : "Get Problem"}
-        </button>
-      </div>
-
-      {current && (
-        <div className="card">
-          <div className="card-header">
-            <span className="problem-number">#{current.id}</span>
-            <h2 className="problem-title">{current.title}</h2>
-          </div>
-
-          <div className="section">
-            <span className="label">Implement</span>
-            <pre className="code">{current.signature}</pre>
-          </div>
-
-          <div className="section">
-            <span className="label">Input</span>
-            <pre className="code">{current.input}</pre>
-          </div>
-
-          <div className="section">
-            <span className="label">Output</span>
-            <pre className="code">{current.output}</pre>
-          </div>
-
-          <div className="section">
-            <span className="label">Tests</span>
-            <ul className="tests-list">
-              {current.tests.map((t) => (
-                <li key={t}>{t}</li>
-              ))}
-            </ul>
-          </div>
+    <>
+      <div className={`app${current ? " app--started" : ""}`}>
+        <div className="header">
+          <h1>Algorithm Problem Randomizer</h1>
+          <button className="roll-btn" onClick={getRandomProblem}>
+            {current ? "Next Problem" : "Get Problem"}
+          </button>
         </div>
-      )}
-    </div>
+
+        {current && (
+          <div className="card">
+            <div className="card-header">
+              <span className="problem-number">#{current.id}</span>
+              <h2 className="problem-title">{current.title}</h2>
+            </div>
+
+            <div className="section">
+              <span className="label">Implement</span>
+              <pre className="code">{current.signature}</pre>
+            </div>
+
+            <div className="section">
+              <span className="label">Input</span>
+              <pre className="code">{current.input}</pre>
+            </div>
+
+            <div className="section">
+              <span className="label">Output</span>
+              <pre className="code">{current.output}</pre>
+            </div>
+
+            <div className="section">
+              <span className="label">Tests</span>
+              <ul className="tests-list">
+                {current.tests.map((t) => (
+                  <li key={t}>{t}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+        <div className="editor-wrapper">
+          <iframe
+            src="https://onecompiler.com/embed/javascript?theme=dark&hideTitle=true&hideNew=true"
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
